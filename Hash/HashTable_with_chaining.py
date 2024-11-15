ade7a97c129b75cc02a7f10eac68fc1c20a91558
ntwalibruno <nbahongere30541@ucumberlands.edu>
2024-11-15 15:49:20 -0600

    HashTable implementation with Chainining

---
+++ Hash/HashTable_with_chaining.py
@@ -0,0 +1,45 @@
+class HashTable:
+    def __init__(self, size=10):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+
+    def hash(self, key):
+        return hash(key) % self.size
+
+    def InsertItem(self, key, value):
+        index = self.hash(key)
+        self.table[index].append((key, value))
+
+    def DeleteItem(self, key):
+        index = self.hash(key)
+
+        for idx, k in enumerate(self.table[index]):
+            if k == key:
+                del self.table[index][idx]
+                return True  
+        return False  
+        
+    def Search(self, key):
+        index = self.hash(key)
+        for k, value in self.table[index]:
+            if k == key:
+                return value
+            
+        return None
+
+    def display_hash(self):
+        print(str(self.table))
+
+if __name__ == "__main__":
+    a = [15, 11, 27, 8, 12]
+
+    hashTable = HashTable()
+    
+    hashTable.InsertItem(1, "House")
+    hashTable.InsertItem(2, "Apartment")
+    hashTable.InsertItem(2, "Condo")
+    hashTable.InsertItem("Room", 5)
+
+    hashTable.DeleteItem("Room")
+
+    hashTable.display_hash()
